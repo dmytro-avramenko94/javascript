998d04b86b6acbd54d39dace85e2f596265579a3
Dmytro Avramenko <avram2705@gmail.com>
2024-02-07 22:19:41 +0100

    homework-02 (without last one max)

--- lesson-02(data-types)/homework/main.js
+++ lesson-02(data-types)/homework/main.js
@@ -36,3 +36,43 @@ btn3.onclick = () => {
     res3_1.innerHTML = Math.floor(inp3 * 1000 / 820)
     res3_2.innerHTML = inp3 * 1000 % 820
 }
+
+//Normal 1
+let btn4 = document.querySelector('.btn-4')
+
+btn4.onclick = () => {
+    let inp4_1 = document.querySelector('.inp-norm-11')
+    let inp4_2 = document.querySelector('.inp-norm-12')
+    let res4_1 = document.querySelector('.norm-1-1-result')
+    let res4_2 = document.querySelector('.norm-1-2-result')
+
+    res4_1.innerHTML = Math.floor(inp4_1.value / inp4_2.value) + " chocolates"
+    res4_2.innerHTML = inp4_1.value % inp4_2.value
+}
+
+//Normal 2
+let btn5 = document.querySelector('.btn-5')
+
+btn5.onclick = () => {
+    let inp5 = document.querySelector('.inp-norm-2')
+    let res5 = document.querySelector('.norm-2-1-result')
+    let calc1 = Math.floor(inp5.value / 100)
+    let calc2 = Math.floor((inp5.value - (calc1 * 100)) / 10)
+    let calc3 = inp5.value - (calc1 * 100) - (calc2 * 10)
+
+    res5.innerHTML = `${calc3}` + `${calc2}` + `${calc1}`
+}
+
+//Maximum 1
+let btn6 = document.querySelector('.btn-6')
+
+btn6.onclick = () => {
+    let inp61 = document.querySelector('.inp-max-1')
+    let inp62 = document.querySelector('.inp-max-2')
+    let res6 = document.querySelector('.max-1-result')
+    let interest = Number(1) + Number(5 / 12 * inp62.value / 100) //Розрахування коефіцієнту, щоб отримати число без відсотків
+    let calc4
+
+    calc4 = inp61.value * interest
+    res6.innerHTML = calc4.toFixed(2)
+}
