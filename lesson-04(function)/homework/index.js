f3bac21c96545c4c4ad160b8759fc884e0750b46
Dmytro Avramenko <avram2705@gmail.com>
2024-02-12 22:19:40 +0100

    homework-04 added (withour normal)

---
+++ lesson-04(function)/homework/index.js
@@ -0,0 +1,59 @@
+//Task minimum - 1
+// function functName() { }
+// btn.onclick = () => { }
+// let Test = function () { }
+
+//Task minimum - 2
+
+function min2(a, b, c) {
+    return min2.length
+}
+console.log(min2.length)
+
+//Task minimum - 3
+
+function min3(x, y) {
+    if (x > y) {
+        return '1'
+    }
+    else if (x == y) {
+        return '0'
+    }
+    else {
+        return '-1'
+    }
+}
+console.log(min3(10, 10));
+
+//Task minimum - 4
+
+let factorial = function min4(n) {
+    if (n < 2) {
+        return '1'
+    }
+    else {
+        return n * min4(n - 1)
+    }
+
+
+}
+console.log(factorial(5));
+
+//Task minimum - 5
+
+function min5(d, e, f) {
+    return String(d) + String(e) + String(f)
+}
+console.log(min5(1, 4, 9));
+
+//Task minimum - 6
+
+function min6(l, w) {
+    if (l, w) {
+        return l * w
+    }
+    else {
+        return l * l
+    }
+}
+console.log(min6(4));
