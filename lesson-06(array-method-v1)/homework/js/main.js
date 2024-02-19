168ea694af18c1fcb08f2eb4f79eb575a9deafc4
Dmytro Avramenko <avram2705@gmail.com>
2024-02-19 22:08:07 +0100

    lesson-06 (homework - not finished)

--- lesson-06(array-method-v1)/homework/js/main.js
+++ lesson-06(array-method-v1)/homework/js/main.js
@@ -11,14 +11,14 @@ const cars = [
         img: './img/m135.webp',
         series: 'Series 1',
         model: 'M135i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 50000,
     },
     {
         img: './img/230i.avif',
         series: 'Series 2',
         model: '230i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 40000,
     },
     {
@@ -32,14 +32,14 @@ const cars = [
         img: './img/m240i.jpg',
         series: 'Series 2',
         model: 'M240i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 74000,
     },
     {
         img: './img/320i',
         series: 'Series 3',
         model: '320i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 44000,
     },
     {
@@ -53,14 +53,14 @@ const cars = [
         img: './img/m340i.webp',
         series: 'Series 3',
         model: 'M340i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 83000,
     },
     {
         img: './img/420i-coupe',
         series: 'Series 4',
         model: '420i Coupe',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 50000,
     },
     {
@@ -74,14 +74,14 @@ const cars = [
         img: './img/m4-csl.avif',
         series: 'Series 4',
         model: 'M4 CSL',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 100000,
     },
     {
         img: './img/530i.jpg',
         series: 'Series 5',
         model: '530i',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 60000,
     },
     {
@@ -95,8 +95,125 @@ const cars = [
         img: './img/m5.jpg',
         series: 'Series 5',
         model: 'M5',
-        fuel: 'benzine',
+        fuel: 'petrol',
         price: 120000,
     },
 ]
+
+let out1 = document.querySelector('.out1')
+let out2 = document.querySelector('.out2')
+let out3 = document.querySelector('.out3')
+let out4 = document.querySelector('.out4')
+let out5 = document.querySelector('.out5')
+let inp1 = document.querySelector('.selects__price-sort-01')
+let inp2 = document.querySelector('.selects__price-sort-02')
+let btn = document.querySelector('.selects__btn')
+let seriesSelect = document.querySelector('.selects__series')
+let fuelTypeSelect = document.querySelector('.selects__fuel-type')
+
+
+
+
+cars.map(car => {
+
+    let createCard = function () {
+        let card = document.createElement('div')
+        let carImg = document.createElement('img')
+        carImg.src = car.img
+        carImg.width = 300
+        let cardTitle = document.createElement('h3')
+        let cardFuel = document.createElement('p')
+        let cardPrice = document.createElement('p')
+        card.append(carImg, cardTitle, cardFuel, cardPrice)
+        cardTitle.innerHTML = 'BMW' + ' ' + car.model + ', ' + car.series
+        cardFuel.innerHTML = car.fuel
+        cardPrice.innerHTML = car.price + ' €'
+
+        return card
+    }
+
+
+    let card = createCard()
+    if (car.series == 'Series 1') {
+        out1.append(card)
+    }
+    else if (car.series == 'Series 2') {
+        out2.append(card)
+    }
+    else if (car.series == 'Series 3') {
+        out3.append(card)
+    }
+    else if (car.series == 'Series 4') {
+        out4.append(card)
+    }
+    else if (car.series == 'Series 5') {
+        out5.append(card)
+    }
+
+})
+
+//Robymo select Fuel bez powtoren
+let uniqueFuel = new Set()
+cars.forEach(car => {
+    uniqueFuel.add(car.fuel)
+})
+uniqueFuel.forEach(fuel => {
+    let optFuel = document.createElement('option')
+    optFuel.innerHTML = fuel
+    fuelTypeSelect.append(optFuel)
+})
+
+//robymo sortuwannia
+btn.onclick = () => {
+    cars.map(car => {
+        let createCard = function () {
+            let card = document.createElement('div')
+            let carImg = document.createElement('img')
+            carImg.src = car.img
+            carImg.width = 300
+            let cardTitle = document.createElement('h3')
+            let cardFuel = document.createElement('p')
+            let cardPrice = document.createElement('p')
+            card.append(carImg, cardTitle, cardFuel, cardPrice)
+            cardTitle.innerHTML = 'BMW' + ' ' + car.model + ', ' + car.series
+            cardFuel.innerHTML = car.fuel
+            cardPrice.innerHTML = car.price + ' €'
+
+            return card
+        }
+
+        //sortuwannia po fuel
+        let selectedFuel = fuelTypeSelect.value
+        out1.innerHTML = ''
+        out2.innerHTML = ''
+        out3.innerHTML = ''
+        out4.innerHTML = ''
+        out5.innerHTML = ''
+
+        let filteredByFuelCars = cars.filter(car => car.fuel == selectedFuel)
+        console.log(filteredByFuelCars);
+
+        filteredByFuelCars.forEach(filteredByFuelCar => {
+            let card = createCard()
+            if (filteredByFuelCar.series == 'Series 1') {
+                out1.append(card)
+            }
+            else if (filteredByFuelCar.series == 'Series 2') {
+                out2.append(card)
+            }
+            else if (filteredByFuelCar.series == 'Series 3') {
+                out3.append(card)
+            }
+            else if (filteredByFuelCar.series == 'Series 4') {
+                out4.append(card)
+            }
+            else if (filteredByFuelCar.series == 'Series 5') {
+                out5.append(card)
+            }
+        })
+    })
+
+
+
+}
 
